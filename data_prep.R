613d1a3eb5facd402298b07e3bd2466dcfdaba45
nrbjerg <mart547n@outlook.dk>
2022-11-03 09:11:21 +0100

    til engelsk

--- data_prep.R
+++ data_prep.R
@@ -1,17 +1,55 @@
-load(file='cityhomes.Rda')
 df <- cityhomes # Shorthand from now on.
 
 # Adds fin trend, which is trend + 1/12 * month_number.
 df$Salgsmaaned_encoded <- as.numeric(factor(df$Salgsmaaned, levels = unique(df$Salgsmaaned), exclude = NULL))
 df$Fin_trend <- df$Trend +  1/12 * df$Salgsmaaned_encoded
-df$Log_Pris <- log(df$Pris_Salg)
-df$Log_Areal_Bolig <- log(df$Areal_Bolig)
-df$Log_Areal_Grund <- log(df$Areal_Grund)
-df$Log_Alder <- sqrt(df$Alder)
 
-save(one_hot_encoded, file='cityhomes.Rda')
+# Convert column names to english
+df$Price <- df$Pris_Salg
+df$Rooms <- df$Ejd_AntalRum
+df$Year <- df$Salgsaar
+df$Month <- df$Salgsmaaned
+df$Ground_Areal <- df$Areal_Grund
+df$Home_Areal <- df$Areal_Bolig
+df$Big_Ground <- df$StorGrund
+df$Voting_Area <- df$Afstemningsomraade
+df$Distance_School <- df$Dist_skole
+df$Distance_City_Hall <- df$Dist_raadhus
+df$Age <- df$Alder
+df$Parish <- df$Sogn
+df$Walthy <- df$Velhavende
+df$Municipality <- df$KommuneNavn
 
-mod <- lm(Log_Pris~Log_Areal_Bolig + Log_Areal_Grund + Ejd_AntalRum + Fin_trend + StorGrund + Sogn + Dist_skole + Dist_raadhus + Log_Alder + Velhavende, data=df)
-summary(mod)$r.squared
-summary(mod)$adj.r.squared
+drop <- c("Pris_Salg","Areal_Bolig", "Areal_Grund", "Ejd_AntalRum", "Salgsmaaned", "StorGrund", "Dist_skole", "Dist_raadhus", "Dist_Town_Hall", "KommuneNavn", "Velhavende", "Alder", "Dist_raadhus", "Dist_skole", "Sogn", "Afstemningsomraade", "Salgsaar", "Salgsmaaned_encoded")
+df = df[,!(names(df) %in% drop)]
 
+library(stringr)
+
+# Convert the data point strings to english 
+df$Municipality <- str_replace(df$Municipality, "København", "Copenhagen")
+
+df$Parish <- str_replace(df$Parish , "å", "aa")
+df$Parish <- str_replace(df$Parish , "ø", "oe")
+
+df$Voting_Area <- str_replace(df$Voting_Area , "å", "aa")
+df$Voting_Area <- str_replace(df$Voting_Area , "ø", "oe")
+df$Voting_Area <- str_replace(df$Voting_Area , "Ø", "Oe")
+df$Voting_Area <- str_replace(df$Voting_Area , "æ", "ae")
+df$Voting_Area <- str_replace(df$Voting_Area, "Nordvest", "Northwest")
+df$Voting_Area <- str_replace(df$Voting_Area, "Vest", "West")
+df$Voting_Area <- str_replace(df$Voting_Area, "Nord", "North")
+
+
+df$Month
+df$Month <- str_replace(df$Month, "Januar", "January")
+df$Month <- str_replace(df$Month, "Februar", "February")
+df$Month <- str_replace(df$Month, "Marts", "March")
+df$Month <- str_replace(df$Month, "Maj", "May")
+df$Month <- str_replace(df$Month, "Juni", "June")
+df$Month <- str_replace(df$Month, "Juli", "July")
+df$Month <- str_replace(df$Month, "Oktober", "October")
+
+
+
+# Areal_grund + StorGrund: 66, 271, 493
+# Ejd_AntalRum: 28, 113, 115, 560, 727, 806
